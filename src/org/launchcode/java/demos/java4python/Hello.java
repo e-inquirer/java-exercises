fecdb06cde249feeb87c296b60c0dedb51cc4f2b
e-inquirer <sorour.a@gmail.com>
2017-03-01 18:30:11 -0500

    Class 1 Prep Exercises: Input/Output

--- src/org/launchcode/java/demos/java4python/Hello.java
+++ src/org/launchcode/java/demos/java4python/Hello.java
@@ -1,10 +1,16 @@
 package org.launchcode.java.demos.java4python;
-
+import java.util.Scanner;
 /**
  * From "Java for Python Programmers"
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        Scanner in = new Scanner(System.in);
+        String name;
+
+        System.out.print("Enter your name: ");
+        name = in.next();
+
+        System.out.println("Hello " + name);
     }
 }
