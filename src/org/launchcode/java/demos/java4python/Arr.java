4cf3562cac3f6b2ff6ee4c8090bbd7ed6fafb68a
e-inquirer <sorour.a@gmail.com>
2017-03-02 17:04:19 -0500

    Class 2 Prep Exercises: 3. Arrays

---
+++ src/org/launchcode/java/demos/java4python/Arr.java
@@ -0,0 +1,21 @@
+package org.launchcode.java.demos.java4python;
+
+/**
+ * Class 2 Prep Exercises
+ */
+public class Arr {
+
+    public static void main(String[] args) {
+
+        int maxNums = 6;
+        // Initialize integer array
+        int[] someInts = {1, 1, 2, 3, 5, 8};
+
+
+        // Print out integers in array
+        for (int entry : someInts) {
+            System.out.println(entry);
+        }
+    }
+}
+
